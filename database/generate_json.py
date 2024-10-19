28f5dd4ce7953e38e8e0d4c231aa88c8948952cf
Yakobus Iryanto Prasethio <yakobusiryantoprasethio@gmail.com>
2024-10-19 20:00:08 +0700

    feat: new cached database seed

--- database/generate_json.py
+++ database/generate_json.py
@@ -1,22 +1,36 @@
 import asyncio
-import aiohttp
 import time
+import json
+import os
 from datetime import datetime
 from jikanpy import AioJikan
 from rich.console import Console
-from rich.pretty import pprint
+import random
 
 console = Console()
 
-success_count = 0
-failure_count = 0
-failed_ids = []
+
+def read_ids_from_file(file_path):
+    with open(file_path, "r") as file:
+        return [int(line.strip()) for line in file.readlines()]
 
 
-async def create_anime(session, anime_data, anime_episodes, embed_url):
-    global success_count, failure_count, failed_ids
+def save_payload_to_json(filename, payload):
+    os.makedirs("media_cache", exist_ok=True)
+    filepath = f"media_cache/{filename}.json"
+    with open(filepath, "w") as file:
+        json.dump(payload, file, indent=2)
+    console.print(f"Payload saved to {filepath}", style="blue")
 
-    anime_payload = {
+
+def create_anime(anime_data, anime_episodes, embed_url):
+    year = random.randint(2020, 2024)
+    month = random.randint(1, 12)
+    day = 1
+    random_date = datetime(year, month, day)
+    consumed_at = random_date.strftime("%Y-%m-%dT00:00:00.000Z")
+
+    return {
         "malId": anime_data["mal_id"],
         "type": anime_data["type"],
         "status": anime_data["status"],
@@ -26,9 +40,7 @@ async def create_anime(session, anime_data, anime_episodes, embed_url):
         else None,
         "title": anime_data["title"],
         "titleJapanese": anime_data["title_japanese"],
-        "titleSynonyms": " ".join(
-            [synonym.lower() for synonym in anime_data["title_synonyms"]]
-        ),
+        "titleSynonyms": " ".join([synonym.lower() for synonym in anime_data["title_synonyms"]]),
         "source": anime_data["source"],
         "aired": (
             f"{datetime.strptime(anime_data["aired"]["from"], "%Y-%m-%dT%H:%M:%S%z").strftime("%b %d, %Y")} to "
@@ -56,9 +68,9 @@ async def create_anime(session, anime_data, anime_episodes, embed_url):
         "themes": [theme["name"] for theme in anime_data["themes"]],
         "episodes": [
             {
-                "aired": datetime.strptime(
-                    episode["aired"], "%Y-%m-%dT%H:%M:%S%z"
-                ).strftime("%b %d, %Y")
+                "aired": datetime.strptime(episode["aired"], "%Y-%m-%dT%H:%M:%S%z").strftime(
+                    "%b %d, %Y"
+                )
                 if episode["aired"]
                 else "N/A",
                 "number": episode["mal_id"],
@@ -71,46 +83,28 @@ async def create_anime(session, anime_data, anime_episodes, embed_url):
         "synopsis": anime_data["synopsis"],
         "trailer": embed_url,
         "malUrl": anime_data["url"],
+        "storylineRating": random.randint(1, 10),
+        "qualityRating": random.randint(1, 10),
+        "voiceActingRating": random.randint(1, 10),
+        "soundTrackRating": random.randint(1, 10),
+        "charDevelopmentRating": random.randint(1, 10),
+        "consumedAt": consumed_at,
     }
 
-    backend_url = "http://localhost:3000/api/anime"
 
-    try:
-        async with session.post(backend_url, json=anime_payload) as response:
-            if response.status == 201:
-                console.print(
-                    f"Anime [bold]{anime_data['title']}[/bold] created successfully!",
-                    style="green",
-                )
-                success_count += 1
-            else:
-                console.print(
-                    f"Failed to create anime {anime_data['title']}: {response.status}",
-                    style="red",
-                )
-                failure_count += 1
-                failed_ids.append(anime_data["mal_id"])
-    except Exception as e:
-        console.print(
-            f"Error while creating anime {anime_data['title']}: {str(e)}", style="red"
-        )
-        failure_count += 1
-        failed_ids.append(anime_data["mal_id"])
+def create_manga(manga_data):
+    year = random.randint(2020, 2024)
+    month = random.randint(1, 12)
+    day = 1
+    random_date = datetime(year, month, day)
+    consumed_at = random_date.strftime("%Y-%m-%dT00:00:00.000Z")
 
-    print("\n")
-
-
-async def create_manga(session, manga_data):
-    global success_count, failure_count, failed_ids
-
-    manga_payload = {
+    return {
         "malId": manga_data["mal_id"],
         "status": manga_data["status"],
         "title": manga_data["title"],
         "titleJapanese": manga_data["title_japanese"],
-        "titleSynonyms": " ".join(
-            [synonym.lower() for synonym in manga_data["title_synonyms"]]
-        )
+        "titleSynonyms": " ".join([synonym.lower() for synonym in manga_data["title_synonyms"]])
         if manga_data["title_synonyms"]
         else "",
         "published": (
@@ -138,38 +132,23 @@ async def create_manga(session, manga_data):
         "themes": [theme["name"] for theme in manga_data["themes"]],
         "synopsis": manga_data["synopsis"],
         "malUrl": manga_data["url"],
+        "storylineRating": random.randint(1, 10),
+        "artStyleRating": random.randint(1, 10),
+        "charDevelopmentRating": random.randint(1, 10),
+        "worldBuildingRating": random.randint(1, 10),
+        "originalityRating": random.randint(1, 10),
+        "consumedAt": consumed_at,
     }
 
-    backend_url = "http://localhost:3000/api/manga"
 
-    try:
-        async with session.post(backend_url, json=manga_payload) as response:
-            if response.status == 201:
-                console.print(
-                    f"Manga [bold]{manga_data['title']}[/bold] created successfully!",
-                    style="green",
-                )
-                success_count += 1
-            else:
-                console.print(
-                    f"Failed to create manga {manga_data['title']}: {response.status}",
-                    style="red",
-                )
-                failure_count += 1
-                failed_ids.append(manga_data["mal_id"])
-                pprint(manga_payload)
-    except Exception as e:
-        console.print(
-            f"Error while creating anime {manga_data['title']}: {str(e)}", style="red"
-        )
-        failure_count += 1
-        failed_ids.append(manga_data["mal_id"])
+def create_lightnovel(lightnovel_data):
+    year = random.randint(2020, 2024)
+    month = random.randint(1, 12)
+    day = 1
+    random_date = datetime(year, month, day)
+    consumed_at = random_date.strftime("%Y-%m-%dT00:00:00.000Z")
 
-
-async def create_lightnovel(session, lightnovel_data):
-    global success_count, failure_count, failed_ids
-
-    lightnovel_payload = {
+    return {
         "malId": lightnovel_data["mal_id"],
         "status": lightnovel_data["status"],
         "title": lightnovel_data["title"],
@@ -204,174 +183,38 @@ async def create_lightnovel(session, lightnovel_data):
         "themes": [theme["name"] for theme in lightnovel_data["themes"]],
         "synopsis": lightnovel_data["synopsis"],
         "malUrl": lightnovel_data["url"],
+        "storylineRating": random.randint(1, 10),
+        "worldBuildingRating": random.randint(1, 10),
+        "writingStyleRating": random.randint(1, 10),
+        "charDevelopmentRating": random.randint(1, 10),
+        "originalityRating": random.randint(1, 10),
+        "consumedAt": consumed_at,
     }
-
-    backend_url = "http://localhost:3000/api/lightnovel"
-
-    try:
-        async with session.post(backend_url, json=lightnovel_payload) as response:
-            if response.status == 201:
-                console.print(
-                    f"Light Novel [bold]{lightnovel_data['title']}[/bold] created successfully!",
-                    style="green",
-                )
-                success_count += 1
-            else:
-                console.print(
-                    f"Failed to create light novel {lightnovel_data['title']}: {response.status}",
-                    style="red",
-                )
-                failure_count += 1
-                failed_ids.append(lightnovel_data["mal_id"])
-                pprint(lightnovel_payload)
-    except Exception as e:
-        console.print(
-            f"Error while creating anime {lightnovel_data['title']}: {str(e)}",
-            style="red",
-        )
-        failure_count += 1
-        failed_ids.append(lightnovel_data["mal_id"])
 
 
 async def main():
-    global success_count, failure_count, failed_ids
     start_time = time.time()
 
-    anime_ids = [
-        54744,
-        50803,
-        52481,
-        54968,
-        51122,
-        54492,
-        41457,
-        48569,
-        48675,
-        51705,
-        22147,
-        38778,
-        37475,
-        47917,
-        40530,
-        15583,
-        19163,
-        36633,
-        41461,
-        51417,
-        38680,
-        40417,
-        49310,
-        42938,
-        37105,
-        56230,
-        49692,
-        40938,
-        11617,
-        24703,
-        34281,
-        15451,
-        42897,
-        54856,
-        31338,
-        36098,
-        31245,
-        50796,
-        37999,
-        43608,
-        40591,
-        6045,
-        48926,
-        50631,
-        38080,
-        38889,
-        51815,
-        50709,
-        7054,
-        33487,
-        48736,
-        51552,
-        51265,
-        53126,
-        49470,
-        14813,
-        39547,
-        23847,
-        18897,
-        27787,
-        20507,
-        30503,
-        7769,
-        30276,
-        34134,
-        38483,
-        52990,
-        40595,
-        36220,
-        37450,
-        52619,
-        40839,
-        42963,
-        52082,
-        38992,
-        43470,
-        38787,
-        45613,
-        50416,
-        30123,
-        12549,
-        50265,
-        47194,
-        50594,
-        50739,
-        16782,
-        47159,
-        54234,
-        51916,
-        33036,
-        38101,
-        39783,
-        48548,
-        50593,
-        49776,
-        52305,
-        41389,
-        4224,
-        34822,
-        34902,
-    ]
+    anime_ids = read_ids_from_file("anime_ids.txt")
+    manga_ids = read_ids_from_file("manga_ids.txt")
+    lightnovel_ids = read_ids_from_file("lightnovel_ids.txt")
 
-    manga_ids = [
-        123602,
-        156291,
-        112589,
-        147082,
-        130633,
-        142815,
-        164769,
-        143031,
-        147528,
-        45143,
-        152087,
-        115848,
-        139073,
-        128634,
-        125052,
-        152117,
-        121433,
-        160983,
-        108407,
-    ]
+    anime_payloads = []
+    manga_payloads = []
+    lightnovel_payloads = []
 
-    lightnovel_ids = [133757, 133352, 102702, 135688, 132505, 25409, 123649]
+    anime_ids_length = len(anime_ids)
+    manga_ids_length = len(manga_ids)
+    lightnovel_ids_length = len(lightnovel_ids)
 
-    async with AioJikan() as aio_jikan, aiohttp.ClientSession() as session:
-        for anime_id in anime_ids:
+    async with AioJikan() as aio_jikan:
+        for index, anime_id in enumerate(anime_ids, start=1):
             try:
                 # Fetch anime data
                 anime_data = await aio_jikan.anime(anime_id)
                 anime_data = anime_data["data"]
                 console.print(
-                    f"Processing [bold]{anime_data["title"]}[/bold]...", style="blue"
+                    f"[ANIME] Processing [bold]{anime_data["title"]}[/bold]...", style="blue"
                 )
 
                 # Fetch anime episodes
@@ -380,78 +223,84 @@ async def main():
 
                 # Embed URL fix
                 embed_url = (
-                    anime_data["trailer"]["embed_url"].replace(
-                        "autoplay=1", "autoplay=0"
-                    )
+                    anime_data["trailer"]["embed_url"].replace("autoplay=1", "autoplay=0")
                     if anime_data["trailer"]
                     else None
                 )
 
-                # Create anime in the backend
-                await create_anime(session, anime_data, anime_episodes, embed_url)
+                # Create anime payload
+                anime_payload = create_anime(anime_data, anime_episodes, embed_url)
+                anime_payloads.append(anime_payload)
+                console.print(
+                    f"({index}/{anime_ids_length}): [ANIME] [bold]{anime_data['title']}[/bold] added successfully!",
+                    style="green",
+                )
             except Exception as e:
                 console.print(
                     f"Error fetching or processing anime ID [bold]{anime_id}[/bold]: {str(e)}",
                     style="red",
                 )
-                failure_count += 1
-                failed_ids.append(anime_id)
 
             await asyncio.sleep(3)
 
-        for manga_id in manga_ids:
+        save_payload_to_json("anime_payloads", anime_payloads)
+
+        for index, manga_id in enumerate(manga_ids, start=1):
             try:
                 # Fetch manga data
                 manga_data = await aio_jikan.manga(manga_id)
                 manga_data = manga_data["data"]
                 console.print(
-                    f"Processing [bold]{manga_data["title"]}[/bold]...", style="blue"
+                    f"[MANGA] Processing [bold]{manga_data["title"]}[/bold]...", style="blue"
                 )
 
-                # Create manga in the backend
-                await create_manga(session, manga_data)
+                # Create manga payload
+                manga_payload = create_manga(manga_data)
+                manga_payloads.append(manga_payload)
+                console.print(
+                    f"({index}/{manga_ids_length}): [MANGA] [bold]{manga_data['title']}[/bold] added successfully!",
+                    style="green",
+                )
             except Exception as e:
                 console.print(
                     f"Error fetching or processing manga ID [bold]{manga_id}[/bold]: {str(e)}",
                     style="red",
                 )
-                failure_count += 1
-                failed_ids.append(manga_id)
 
             await asyncio.sleep(3)
 
-        for lightnovel_id in lightnovel_ids:
+        save_payload_to_json("manga_payloads", manga_payloads)
+
+        for index, lightnovel_id in enumerate(lightnovel_ids, start=1):
             try:
                 # Fetch lightnovel data
                 lightnovel_data = await aio_jikan.manga(lightnovel_id)
                 lightnovel_data = lightnovel_data["data"]
                 console.print(
-                    f"Processing [bold]{lightnovel_data["title"]}[/bold]...",
+                    f"[LIGHTNOVEL] Processing [bold]{lightnovel_data["title"]}[/bold]...",
                     style="blue",
                 )
 
-                # Create lightnovel in the backend
-                await create_lightnovel(session, lightnovel_data)
+                # Create lightnovel payload
+                lightnovel_payload = create_lightnovel(lightnovel_data)
+                lightnovel_payloads.append(lightnovel_payload)
+                console.print(
+                    f"({index}/{lightnovel_ids_length}): [LIGHTNOVEL] [bold]{lightnovel_data['title']}[/bold] added successfully!",
+                    style="green",
+                )
             except Exception as e:
                 console.print(
                     f"Error fetching or processing light novel ID [bold]{lightnovel_id}[/bold]: {str(e)}",
                     style="red",
                 )
-                failure_count += 1
-                failed_ids.append(lightnovel_id)
 
             await asyncio.sleep(3)
 
-    # Summary at the end
-    console.print("\nSummary:", style="bold yellow")
-    console.print(f"Successful media creations: {success_count}", style="green")
-    console.print(f"Failed media creations: {failure_count}", style="red")
-    if failed_ids:
-        console.print(f"Failed media IDs: {failed_ids}", style="red")
+        save_payload_to_json("lightnovel_payloads", lightnovel_payloads)
 
     end_time = time.time()
     elapsed_time = end_time - start_time
-    console.print(f"Time elapsed: {elapsed_time} seconds", style="blue")
+    console.print(f"Time elapsed: {round(elapsed_time, 2)} seconds", style="blue")
 
 
 if __name__ == "__main__":
---
+++ database/seed_backend.py
@@ -0,0 +1,67 @@
+import os
+import json
+import aiohttp
+import asyncio
+from rich.console import Console
+
+console = Console()
+success_count = 0
+failure_count = 0
+failed_ids = []
+
+
+async def post_payload(session: aiohttp.ClientSession, payload, media_type: str):
+    global success_count, failure_count, failed_ids
+    backend_url = f"http://localhost:3000/api/{media_type}"
+
+    try:
+        async with session.post(backend_url, json=payload) as response:
+            if response.status == 201:
+                console.print(
+                    f"[{media_type.upper()}]: [bold]{payload['title']}[/bold] {media_type} created successfully!",
+                    style="green",
+                )
+                success_count += 1
+            else:
+                console.print(
+                    f"Failed to create {media_type} {payload['title']}: {response.status}",
+                    style="red",
+                )
+                failure_count += 1
+                failed_ids.append(payload["mal_id"])
+    except Exception as e:
+        console.print(
+            f"Error while creating {media_type} {payload['title']}: {str(e)}", style="red"
+        )
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        media_cache_path = "media_cache"
+
+        for filename in os.listdir(media_cache_path):
+            if filename.endswith(".json"):
+                filepath = os.path.join(media_cache_path, filename)
+
+                with open(filepath, "r") as file:
+                    data = json.load(file)
+                    for payload in data:
+                        media_type = (
+                            "anime"
+                            if "anime" in filename
+                            else "manga"
+                            if "manga" in filename
+                            else "lightnovel"
+                        )
+                        await post_payload(session, payload, media_type)
+
+    # Summary at the end
+    console.print("\nSummary:", style="bold yellow")
+    console.print(f"Successful media creations: {success_count}", style="green")
+    console.print(f"Failed media creations: {failure_count}", style="red")
+    if len(failed_ids) > 0:
+        console.print(f"Failed media IDs: {failed_ids}", style="red")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
