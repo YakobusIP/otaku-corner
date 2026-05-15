fd09fda8a0ee34d426419c1ba0ede0970da6175a
Yakobus Iryanto Prasethio <yakobusiryantoprasethio@gmail.com>
2026-05-15 10:04:10 +0700

    refactor: split statistics to multiple file

--- backend-nest/src/statistic/statistic.module.ts
+++ backend-nest/src/statistic/statistic.module.ts
@@ -1,10 +1,24 @@
 import { Module } from "@nestjs/common";
 
+import { StatisticDashboardService } from "@/statistic/statistic-dashboard.service";
+import { StatisticLibraryService } from "@/statistic/statistic-library.service";
+import { StatisticMediaConsumptionService } from "@/statistic/statistic-media-consumption.service";
+import { StatisticOverviewService } from "@/statistic/statistic-overview.service";
+import { StatisticTasteProfileService } from "@/statistic/statistic-taste-profile.service";
+import { StatisticTopRatedService } from "@/statistic/statistic-top-rated.service";
 import { StatisticController } from "@/statistic/statistic.controller";
 import { StatisticService } from "@/statistic/statistic.service";
 
 @Module({
   controllers: [StatisticController],
-  providers: [StatisticService]
+  providers: [
+    StatisticService,
+    StatisticMediaConsumptionService,
+    StatisticOverviewService,
+    StatisticDashboardService,
+    StatisticTopRatedService,
+    StatisticLibraryService,
+    StatisticTasteProfileService
+  ]
 })
 export class StatisticModule {}
